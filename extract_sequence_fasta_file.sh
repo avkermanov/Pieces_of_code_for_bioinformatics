6c731f2a289ace50547338519305197438635379
avkermanov <63013487+avkermanov@users.noreply.github.com>
2020-04-16 00:09:15 +0100

    Update extract_sequence_fasta_file.sh

--- extract_sequence_fasta_file.sh
+++ extract_sequence_fasta_file.sh
@@ -1,5 +1,9 @@
 
 #works for a fasta file with miltiple records. In the example, first record has header ">gi|1", the next is ">gi|2" and so on
 #the extraction allows to get the first record only in the output file
-sed -n '/>gi|1$/,/>gi|2$/p' gi_n.txt | sed -e '$ d > gi_1.fasta
-#if the header is too long, should be replaced before
+
+first add 
+echo >gi|n >> gi_n.txt
+sed -n '/>gi|1$/,/>gi|2$/p' gi_n.txt | sed -e '$ d' > gi_1.fasta
+#if the header is too long, should be replaced before; in general the headers shoud be uniformed to be searched
+# through loops or be replaceble with variables
