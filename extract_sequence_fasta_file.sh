514bdff0b53fa9eaf37d717ee6dccd3f08edceee
avkermanov <63013487+avkermanov@users.noreply.github.com>
2020-06-07 02:07:11 +0100

    Update extract_sequence_fasta_file.sh

--- extract_sequence_fasta_file.sh
+++ extract_sequence_fasta_file.sh
@@ -3,8 +3,8 @@
 #the extraction allows to get the first record only in the output file
 
 #first add the last header line to the file, otherwise the last record will not be extacted
-echo >gi|n >> gi_n.txt
-sed -n '/>gi|1$/,/>gi|2$/p' gi_n.txt | sed -e '$ d' > gi_1.fasta
+
+sed -n '/>gi|1$/,/>gi|2$/p' gi_n.fa | sed -e '$ d' > gi_1.fasta
 #if the header is too long, should be replaced before; in general the headers shoud be uniformed to be searched
 # through loops or be replaceble with variables
 
